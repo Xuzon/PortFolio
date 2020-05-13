c2f1674bc15930ce0d45143e4d935e3e631fe9c2
Xuzon <xuzon69@gmail.com>
2020-05-13 19:26:19 +0200

    removed namespace and form of a legend specific implementation

--- Shaders/OutlineEffect.cs
+++ Shaders/OutlineEffect.cs
@@ -1,67 +1,74 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-namespace form.legend
+/// <summary>
+/// Will apply te OutlineEffect, only in the visible objects if the camera is
+/// setup as It was mean to
+/// </summary>
+[ExecuteInEditMode]
+public class OutlineEffect : MonoBehaviour
 {
-    public class OutlineEffect : MonoBehaviour
+    [SerializeField]
+    protected List<GameObject> visibleObjects = new List<GameObject>();
+    [SerializeField]
+    protected float drawDistance = 0.01f;
+    protected Color outlineColor;
+    public Color OutlineColor
     {
-        protected List<GameObject> affectedObjects = new List<GameObject>();
-        protected Color outlineColor;
-        public Color OutlineColor
+        get => outlineColor;
+        set
         {
-            get => outlineColor;
-            set
-            {
-                outlineMat.SetColor("_Color", value);
-                outlineColor = value;
-            }
+            outlineMat.SetColor("_Color", value);
+            outlineColor = value;
         }
-        protected Camera cam;
-        protected Material outlineMat;
-        protected Vector4 pixelToUV = Vector4.zero;
-        protected int invisibleLayer = 11;
-        protected int toRenderLayer = 12;
+    }
+    protected Camera cam;
+    protected Material outlineMat;
+    protected Vector4 pixelToUV = Vector4.zero;
+    //remember to this to work the camera needs to cull the invisible layer and render
+    protected int invisibleLayer = 11;
+    protected int toRenderLayer = 12;
 
-        protected void Awake()
+    protected void Awake()
+    {
+        outlineMat = new Material(Shader.Find("Custom/OutlineEffectShader"));
+
+    }
+
+    protected void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.depthTextureMode = DepthTextureMode.DepthNormals;
+        outlineColor = Color.black;
+    }
+
+
+    private void OnPreCull()
+    {
+        ChangeLayerObjects(toRenderLayer);
+    }
+
+    private void OnPostRender()
+    {
+        ChangeLayerObjects(invisibleLayer);
+    }
+
+    private void ChangeLayerObjects(int rendLayer)
+    {
+        foreach (var go in visibleObjects)
         {
-            outlineMat = new Material(Shader.Find("Custom/OutlineEffectShader"));
-
+            go.layer = rendLayer;
         }
+    }
 
-        protected void Start()
-        {
-            cam = GetComponent<Camera>();
-            outlineColor = Color.black;
-        }
-
-
-        private void OnPreCull()
-        {
-            ChangeLayerObjects(toRenderLayer);
-        }
-
-        private void OnPostRender()
-        {
-            ChangeLayerObjects(invisibleLayer);
-        }
-
-        private void ChangeLayerObjects(int rendLayer)
-        {
-            foreach(var go in affectedObjects)
-            {
-                go.layer = rendLayer;
-            }
-        }
-
-        private void OnRenderImage(RenderTexture source, RenderTexture destination)
-        {
-            pixelToUV.x = cam.scaledPixelWidth;
-            pixelToUV.y = cam.scaledPixelHeight;
-            outlineMat.SetVector("_PixelToUVFactor", pixelToUV);
-            Graphics.Blit(source, destination, outlineMat);
-        }
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        pixelToUV.x = cam.scaledPixelWidth;
+        pixelToUV.y = cam.scaledPixelHeight;
+        outlineMat.SetVector("_PixelToUVFactor", pixelToUV);
+        outlineMat.SetFloat("_DistanceToDraw", drawDistance);
+        Graphics.Blit(source, destination, outlineMat);
     }
 }
